d9589349b4384b0297aa04245b28309e58504673
PCMS-SPMNET3 <78403871+PCMS-SPMNET3@users.noreply.github.com>
2022-10-27 10:49:12 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,32 @@
-let x_value = 0
-let y_value = 0
 input.onButtonPressed(Button.A, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
 	
 })
+let dx = 1
+let x_value = 0
+let y_value = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        x_value += 1
+        x_value += dx
+        basic.clearScreen()
+        led.plot(x_value, y_value)
+        basic.pause(200)
     }
     if (input.buttonIsPressed(Button.B)) {
         y_value += 1
+        basic.clearScreen()
+        led.plot(x_value, y_value)
+        basic.pause(200)
     }
     if (x_value > 4) {
-        x_value = 0
+        dx = -1
+    }
+    if (x_value < 0) {
+        dx = 1
     }
     if (y_value > 4) {
-        y_value = 0
+        y_value = -1
     }
 })
